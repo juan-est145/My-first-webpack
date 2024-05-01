ee8f1003342a8511b165ca445e56d9fd566897b2
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-01 19:18:18 +0200

    Now I have bundled an image

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import myName from './myName';
 import './style.css';
+import Icon from './icon.png';
 
 
-function component() {
+function component() 
+{
 	const element = document.createElement('div');
   
 	// Lodash, now imported by this script
@@ -11,7 +13,12 @@ function component() {
 
 	element.textContent = myName("Juan");
 	element.classList.add("hello");
+	
+	const myIcon = new Image();
+	myIcon.src = Icon;
+	element.appendChild(myIcon);
+	
 	return element;
-  }
+}
   
-  document.body.appendChild(component());
+document.body.appendChild(component());
